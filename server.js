c5f1be6d6756309fbd3ad0960dbfbcf687c472af
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-03-11 16:15:21 +0200

    Add new header for localhost/diary

--- server.js
+++ server.js
@@ -27,8 +27,15 @@ app.use(bodyParser.json());
 // Add headers
 app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+  var allowedOrigins = ['http://localhost/diary', 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:9000'];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+
+    // // Website you wish to allow to connect
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
