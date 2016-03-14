f0aa481dc8df7a3544e9be47eb9c8d365230f433
Steven Jackson <stevenjackson.sanguine@gmail.com>
2016-03-14 19:07:16 +0200

    Portfolio added to API. Added Node dependencies

---
+++ app/models/portfolio.js
@@ -0,0 +1,22 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var portfolioSchema = new Schema({
+  platform: String,
+  clientName: String,
+  url: String,
+  imgsrc: String,
+  recent: Boolean,
+  date: Date,
+  comments: String
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var portfolio = mongoose.model('Portfolio', portfolioSchema);
+
+// make this available to our users in our Node applications
+module.exports = portfolio;
+
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,4 +1,5 @@
 var Task = require('../models/task');
+var Portfolio = require('../models/portfolio');
 var config = require('../../config');
 var secretKey = config.secretKey;
 
@@ -8,8 +9,12 @@ module.exports = function(app, express){
 
   var api = express.Router();
 
+/*****************************************************
+DAIRY GET AND POST
+*****************************************************/
 
-// Get invoices data from the database
+
+// Get tasks data from the database
 api.get('/tasks', function(req,res){
 
   Task.find({}, function(err, tasks){
@@ -62,8 +67,72 @@ task.save(function(err){
 
 });
 
+/*****************************************************
+END DAIRY GET AND POST
+*****************************************************/
 
 
+/*****************************************************
+PORTFOLIo GET AND POST
+*****************************************************/
+
+
+// Get tasks data from the database
+api.get('/portfolio', function(req,res){
+
+  Portfolio.find({}, function(err, tasks){
+
+    if (err) {
+
+      res.send(err);
+      
+      
+    } else {
+      res.json(tasks);
+
+    }
+
+  });
+
+});
+
+
+
+// Post data to the database
+api.post('/add-portfolio', function(req, res){
+
+// Debug
+// console.log(req.body.url)
+
+
+var portfolio = new Portfolio({
+
+clientName: req.body.clientname,
+  platform: req.body.platform,
+  url: req.body.url,
+  imgsrc: req.body.imgsrc,
+  workDone: req.body.workdone,
+  date: req.body.date,
+  comments: req.body.comments
+});
+
+
+
+portfolio.save(function(err){
+  if (err) {
+    res.send(err);
+    return
+  }
+
+  res.json({message:'Entry saved'});
+
+});
+
+
+});
+/*****************************************************
+END PORTFOLIo GET AND POST
+*****************************************************/
 
 return api;
 
